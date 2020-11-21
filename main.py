6a85cf93c4b9267d6f86662ec3473ab83a3b7b31
hyfung <43932013+hyfung@users.noreply.github.com>
2020-11-21 23:43:31 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import random
+import subprocess
+
+def choose_video(dir):
+    """
+    Takes in a directory, spits a random video with its full path
+    """
+    
+    pass
+
+def main():
+    while True:
+        pass
+    
+if __name__ == '__main__':
+    main()
