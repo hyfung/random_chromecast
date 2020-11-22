371d7b1d879373b6a638290b393cd0764c24043e
hyfung <43932013+hyfung@users.noreply.github.com>
2020-11-22 14:15:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,48 @@
 
 import random
 import subprocess
+import os
+import argparse
 
-def choose_video(dir):
+def choose_video(directory):
     """
     Takes in a directory, spits a random video with its full path
     """
-    
+    files = [file for files in os.listdir(directory) if file.endswith('.mp4')]
+    return directory + random.choice(files)
+
+def cast_video(path):
+    """
+    Takes in the full path to the video, invoke chromecast via VLC via os.system()
+    """
     pass
 
+def delete_video(path):
+    """
+    Takes in the full path to video, delete it
+    """
+
 def main():
+    """
+    This is the entrypoint, depending on the options passed to argparse it will go
+    either one shot or repeatedly
+    """
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dir", help="Full path to the folder", type=str,
+                    required=True, default='/mnt/Media/Youtube')
+    ap.add_argument("-r", "--repeat", help="", action='store_true', default=False)
+    args = vars(ap.parse_args())
+
+    directory = args['dir']
+    if not directory.endswith('/'):
+        directory += '/'
+
     while True:
-        pass
+        file = choose_video(directory)
+        cast_video(file)
+        delete_video(file)
+        if not args['repeat']:
+            exit()
     
 if __name__ == '__main__':
     main()
