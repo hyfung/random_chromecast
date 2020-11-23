b866b0220dad65e1cbe41be643ef31fe4add79bc
Jerry Fung <hyfung@link.cuhk.edu.hk>
2020-11-23 22:21:23 +0800

    Done

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ import subprocess
 import os
 import argparse
 
-def choose_video(directory):
+def choose_video(directory) -> str:
     """
     Takes in a directory, spits a random video with its full path
     """
-    files = [file for files in os.listdir(directory) if file.endswith('.mp4')]
+    if not directory.endswith('/'):
+        directory += '/'
+
+    files = [file for file in os.listdir(directory) if file.endswith('.mp4')]
     return directory + random.choice(files)
 
-def cast_video(path):
+def cast_video(path) -> None:
     """
     Takes in the full path to the video, invoke chromecast via VLC via os.system()
     """
     cmd = 'vlc %s --sout="#chromecast{ip=192.168.0.250}" --demux-filter=demux_chromecast' % path
     os.system(cmd)
 
-def delete_video(path):
+def delete_video(path) -> str:
     """
     Takes in the full path to video, delete it
     """
@@ -32,13 +35,12 @@ def main():
     """
     ap = argparse.ArgumentParser()
     ap.add_argument("-d", "--dir", help="Full path to the folder", type=str,
-                    required=True, default='/mnt/Media/Youtube')
+                    default='/mnt/Media/Youtube')
     ap.add_argument("-r", "--repeat", help="", action='store_true', default=False)
     args = vars(ap.parse_args())
 
     directory = args['dir']
-    if not directory.endswith('/'):
-        directory += '/'
+    
 
     while True:
         file = choose_video(directory)
